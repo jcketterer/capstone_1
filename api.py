5929c9280283fc44e80514056beb285a9409ae8b
JC Ketterer <jcketterer@JCs-MacBook-Pro.local>
2021-12-29 16:10:41 -0700

    messing with api

--- api.py
+++ api.py
@@ -1,35 +1,54 @@
 import requests
-API_URL = "https://api.openbrewerydb.org/breweries?by_type=micro&page=1"
+
+API_URL = "https://api.openbrewerydb.org/breweries"
 
 
-def get_api_response():
-    resp = requests.get(API_URL)
-    data = resp.json()
-    beers = data
-
+def api_response():
+    page = 1
     breweries = []
 
-    for beer in beers:
+    while True:
+        print("----")
+        url = f"{API_URL}?by_state=california&page={page}&per_page=50"
+        print("Requesting url:", url)
 
-        brewery = {
-            'name': beer['name'],
-            'state': beer['state']
-        }
+        resp = requests.get(url)
+        data = resp.json()
 
-        breweries.append(brewery)
-    return breweries
-    # url = API_URL
-    # breweries = []
-    # page = 1
+        if len(data) == 0:
+            break
+        breweries.extend(data)
+        page += 1
 
-    # while True:
-    #     print("*******")
-    #     url = f"https://api.openbrewerydb.org/breweries?by_type=micro&page={page}"
-    #     print("Requesting", url)
 
-    #     resp = requests.get(url)
-    #     data = resp.json()
-    #     if len(data) == 0:
-    #         break
-    #     breweries.extend(data)
-    #     page += 1
+# def get_api_response():
+#     resp = requests.get(API_URL)
+#     data = resp.json()
+#     beers = data
+
+#     breweries = []
+
+#     for beer in beers:
+
+#         brewery = {
+#             'name': beer['name'],
+#             'state': beer['state']
+#         }
+
+#         breweries.append(brewery)
+#     return breweries
+# url = API_URL
+# breweries = []
+# page = 1
+
+# while True:
+#     print("*******")
+#     url = f"https://api.openbrewerydb.org/breweries?by_type=micro&page={page}"
+#     print("Requesting", url)
+
+#     resp = requests.get(url)
+#     data = resp.json()
+#     if len(data) == 0:
+#         break
+#     breweries.extend(data)
+#     page += 1
