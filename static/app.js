2a6036964a14a03ab3299de75fddd9a5ebe36a5a
JC Ketterer <jcketterer18@gmail.com>
2022-01-02 23:01:53 -0700

    changed map to google maps from open street

--- static/app.js
+++ static/app.js
@@ -1,1 +1,20 @@
-//See info.html script tag at the bottom
+// Initialize and add the map
+function initMap() {
+  const lat = document.getElementById('lat').innerText;
+  const long = document.getElementById('long').innerText;
+  console.log(lat);
+  console.log(long);
+
+  // The location of Uluru
+  const uluru = { lat: parseInt(lat), lng: parseInt(long) };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
